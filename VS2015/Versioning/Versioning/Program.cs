ebc1405867b726ba0a483d8fda0d06cab45c8776
ignatandrei <ignatandrei@yahoo.com>
2016-04-16 00:29:50 +0300

    versioning

---
+++ VS2015/Versioning/Versioning/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Versioning
+{
+    /// <summary>    
+    /// read also:
+    /// http://semver.org/
+    /// https://docs.nuget.org/create/versioning 
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var aiv = GetAttribute<AssemblyInformationalVersionAttribute>();
+            var v = Assembly.GetExecutingAssembly().GetName().Version;
+            
+            if (v != null)
+            {
+                Console.Write(v.ToString() + " ");
+            }
+            if (aiv != null)
+            {
+                Console.WriteLine(aiv.InformationalVersion);
+            }
+
+
+        }
+
+        static T GetAttribute<T>()
+            where T:class
+        {
+            return Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(T)).FirstOrDefault() as T;
+        }
+
+    }
+}
