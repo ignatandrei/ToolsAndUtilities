3af2576238e08a6d395458cb784cabf6ec3c82ec
ignatandrei <ignatandrei@yahoo.com>
2016-04-21 09:51:55 +0300

    logging

---
+++ VS2015/Logging/Logging/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace Logging
+{
+    class Program
+    {
+        private static Logger l;
+        static void Main(string[] args)
+        {
+            l = LogManager.GetCurrentClassLogger();
+            try
+            {
+                l.Log(LogLevel.Info, "in main");
+                var textFile = ReadFile("a.txt");
+                Console.WriteLine(textFile);
+            }
+            catch (Exception ex)
+            {
+                l.ErrorException("final error in main ",ex);
+                
+                Console.WriteLine("error happened");
+            }
+        }
+
+        static string ReadFile(string nameFile)
+        {
+            l.Log(LogLevel.Info,nameFile);
+            try
+            {
+                return File.ReadAllText(nameFile);
+            }
+            catch (Exception ex)
+            {
+                l.ErrorException("can not read file "+ nameFile,ex);
+                    
+                throw;
+            }
+            return Environment.CurrentDirectory;
+        }
+    }
+}
