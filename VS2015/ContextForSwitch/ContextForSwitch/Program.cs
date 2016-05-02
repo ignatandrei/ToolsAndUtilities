86e2f6f1550f26e01a1d1bd47b69317fbb868767
ignatandrei <ignatandrei@yahoo.com>
2016-05-02 13:26:12 +0300

    app context

---
+++ VS2015/ContextForSwitch/ContextForSwitch/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SettingSwitch;
+
+namespace ContextForSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SetLicence();
+            var ocr = new GetOCR();
+            Console.WriteLine(ocr.OCRFromImage(null));
+        }
+
+        static void SetLicence()
+        {
+            AppContext.SetSwitch("OCR", true);
+        }
+    }
+}
---
+++ VS2015/ContextForSwitch/SettingSwitch/GetOCR.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SettingSwitch
+{
+    
+    public class GetOCR
+    {
+        public bool IsOCREnabled()
+        {
+            bool test;
+            if (!AppContext.TryGetSwitch("OCR", out test))
+                return false;
+
+            return test;
+        }
+
+        public string OCRFromImage(byte[] image)
+        {
+            if (!IsOCREnabled())
+            {
+                throw new LicenseException(this.GetType(), "you do not have licence for OCR");
+            }
+
+            return "http://msprogrammer.serviciipeweb.ro/";
+        }
+    }
+}
