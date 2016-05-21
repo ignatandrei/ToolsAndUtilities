5a92797b31970546afc966b6f6ae9569b85d9936
ignatandrei <ignatandrei@yahoo.com>
2016-05-21 12:17:30 +0300

    Com and .NET . Example with EXCEL. Calling Excel as it should

---
+++ VS2015/ComTest/ComTest/ExcelTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+
+namespace ComTest
+{
+    public class ExcelTest
+    {
+        public void CreateNew(string fileName)
+        {
+
+            var a=new Application();
+            var w = a.Workbooks.Add();
+            w.SaveAs(fileName);
+            w.Close();
+            a.Quit();
+            w = null;
+            a = null;
+        }
+
+        public void CreateCorrect(string fileName)
+        {
+
+            var a = new Application();
+            var ws = a.Workbooks;
+            var w = ws.Add();
+            w.SaveAs(fileName);
+            w.Close();
+            Marshal.ReleaseComObject(w);
+            w = null;
+            Marshal.ReleaseComObject(ws);
+            ws = null;
+            a.Quit();
+            Marshal.ReleaseComObject(a);            
+            a = null;
+        }
+    }
+}
---
+++ VS2015/ComTest/ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ComTest;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var docs =Path.GetTempPath();
+            var fileName =Path.Combine(docs,"andreiIgnat.xlsx");
+            if(File.Exists(fileName))
+                File.Delete(fileName);
+            var x = new ExcelTest();
+           
+            x.CreateNew(fileName);
+            //x.CreateCorrect(fileName);
+            x = null;
+            Console.WriteLine("See if excel it is in task manager");
+            Console.ReadKey();
+        }
+    }
+}
