dac775871e3f3bb0605007ddcadd723b6bbbdcf2
ignatandrei <ignatandrei@yahoo.com>
2016-05-28 07:51:24 +0300

    ilmerge

---
+++ VS2015/ILMergeObj/ILMergeDOS/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ILMergeObj;
+
+namespace ILMergeDOS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var m = new MyClass();
+            m.WebSite = "http://msprogrammer.serviciipeweb.ro/";
+            Console.WriteLine(m.WebSite);
+            Console.ReadLine();
+        }
+    }
+}
