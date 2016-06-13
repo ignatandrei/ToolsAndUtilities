7ac7fa08113244f9ba4060858cd71672a1130673
ignatandrei <ignatandrei@yahoo.com>
2016-06-13 08:10:16 +0300

    refelction and copy constructor

---
+++ VS2015/CopyConstructorReflection/CopyConstructorReflection/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyConstructorReflection
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string WebSite { get; set; }
+
+        public Person():this(null)
+        {
+            
+        }
+        /// <summary>
+        /// copy constructor for properties
+        /// https://en.wikipedia.org/wiki/Copy_constructor_(C%2B%2B)
+        /// </summary>
+        /// <param name="p"></param>
+        public Person(Person p)
+        {
+            if(p == null)
+                return;
+            //copy properties
+            //this.Name = p.Name;
+            //this.WebSite = p.WebSite;
+            var props = this.GetType().GetProperties();
+            foreach (var propertyInfo in props)
+            {
+                if(!propertyInfo.CanWrite)
+                    continue;
+                if(!propertyInfo.CanRead)
+                    continue;
+
+                var value = propertyInfo.GetGetMethod().Invoke(p, null);
+                propertyInfo.GetSetMethod().Invoke(this, new object[1] {value});
+
+            }
+
+
+        }
+    }
+}
---
+++ VS2015/CopyConstructorReflection/CopyConstructorReflection/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyConstructorReflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p=new Person();
+            p.Name = "Andrei Ignat";
+            p.WebSite = "http://msprogrammer.serviciipeweb.ro/";
+
+            var newPerson=new Person(p);
+            Console.WriteLine(newPerson.WebSite);
+
+        }
+    }
+}
