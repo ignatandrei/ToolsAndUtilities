4bb6d747962a0bd63892eafe5dbd953e62332c97
ignatandrei <ignatandrei@yahoo.com>
2016-06-27 08:57:32 +0300

    selenium

---
+++ VS2015/SeleniumTesting/SeleniumTesting/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTesting
+{
+    class Program
+    {
+        /// <summary>
+        /// https://github.com/SeleniumHQ/selenium
+        /// http://www.seleniumhq.org/download/
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            IWebDriver w=new ChromeDriver();
+            
+            var nav=w.Navigate();
+            nav.GoToUrl("http://msprogrammer.serviciipeweb.ro/about/");
+            var source = w.PageSource;
+            var str = w.Url;
+            Console.WriteLine(str);
+            w.FindElement(By.ClassName("page-item-1892")).Click();
+            str = w.Url;
+            Console.WriteLine(str);
+
+        }
+    }
+}
