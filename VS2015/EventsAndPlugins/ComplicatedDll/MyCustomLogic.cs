dfcbce75c5ed8ae8181b3f411f1d7ef137204aea
ignatandrei <ignatandrei@yahoo.com>
2016-07-24 19:57:42 +0300

    evetns and plugins - ok now

---
+++ VS2015/EventsAndPlugins/ComplicatedDll/MyCustomLogic.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplicatedDll
+{
+    public class MyCustomLogic
+    {
+
+        public bool AffectsCalculus;
+        public event EventHandler<string> beforeCalculus;
+        public IMyCalculusPluginInterface MyCalculusPluginInterface;
+        public bool Calculus(int i, int j)
+        {
+            if (beforeCalculus != null)
+            {
+                beforeCalculus(this, "my custom data");
+            }
+
+            if (MyCalculusPluginInterface != null)
+            {
+                MyCalculusPluginInterface.IBeforeCalculus(this);
+            }
+
+            if (AffectsCalculus)
+            {
+                return i == j;
+            }
+            else
+            {
+                return i - 1 == j + 1;
+            }
+
+        }
+    }
+}
---
+++ VS2015/EventsAndPlugins/EventsAndPlugins/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ComplicatedDll;
+
+namespace EventsAndPlugins
+{
+    class PluginCalculus : IMyCalculusPluginInterface
+    {
+        public void IBeforeCalculus(MyCustomLogic custom)
+        {
+            custom.AffectsCalculus = true;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var m = new MyCustomLogic();
+
+            //m.MyCalculusPluginInterface=new PluginCalculus();
+            //m.beforeCalculus += M_beforeCalculus;
+
+            var result = m.Calculus(1, 1);
+            Console.WriteLine(result);
+        }
+
+        private static void M_beforeCalculus(object sender, string e)
+        {
+            var m = sender as MyCustomLogic;
+            m.AffectsCalculus = true;
+        }
+    }
+}
