1974c7730b8c04e5f61348daa293c8be64b08d78
ignatandrei <ignatandrei@yahoo.com>
2016-09-25 22:26:20 +0300

    see .NET source of any program

---
+++ VS2015/DisAssemble/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Disassemble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string s = $"This thread is  {Thread.CurrentThread.ManagedThreadId} " +
+                       $"user {Thread.CurrentPrincipal.Identity.Name}";
+            Console.WriteLine(s);
+        }
+    }
+}
